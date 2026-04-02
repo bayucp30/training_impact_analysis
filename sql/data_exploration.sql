93c8201b4461b17ce636ecfba716ea360b052b59
bayucp30 <bayucp34@gmail.com>
2026-04-02 16:12:54 +0700

    Update notebook visualization and refine SQL exploration query

--- sql/data_exploration.sql
+++ sql/data_exploration.sql
@@ -1,51 +1,61 @@
 -- USE CASE 1 (Apakah training mempengaruhi performance?)
 
-SELECT ft.employee_id, ft.training_id, dt.training_name, ft.training_date,
+SELECT 
+    ft.employee_id, 
+    ft.training_id, 
+    dt.training_name, 
+    ft.training_date,
+
 -- SEBELUM TRAINING
-    AVG(CASE
+    COALESCE(AVG(CASE
         WHEN fp.date BETWEEN DATE_SUB(ft.training_date, INTERVAL 7 DAY) 
                          AND DATE_SUB(ft.training_date, INTERVAL 1 DAY)
         THEN fp.orders_handled
-    END) as before_orders,
+    END), 0) as before_orders,
 
-    AVG(CASE
+    COALESCE(AVG(CASE
         WHEN fp.date BETWEEN DATE_SUB(ft.training_date, INTERVAL 7 DAY)
                          AND DATE_SUB(ft.training_date, INTERVAL 1 DAY)
         THEN fp.service_time_avg
-    END) as before_service_time,
+    END), 0) as before_service_time,
 
-    AVG(CASE
+    COALESCE(AVG(CASE
         WHEN fp.date BETWEEN DATE_SUB(ft.training_date, INTERVAL 7 DAY)
                          AND DATE_SUB(ft.training_date, INTERVAL 1 DAY)
         THEN fp.error_count
-    END) as before_error,
+    END), 0) as before_error,
 
 -- SETELAH TRAINING
-    AVG(CASE
+    COALESCE(AVG(CASE
         WHEN fp.date BETWEEN DATE_ADD(ft.training_date, INTERVAL 1 DAY)
                          AND DATE_ADD(ft.training_date, INTERVAL 7 DAY)
         THEN fp.orders_handled
-    END) as after_orders,
+    END), 0) as after_orders,
 
-    AVG(CASE
+    COALESCE(AVG(CASE
         WHEN fp.date BETWEEN DATE_ADD(ft.training_date, INTERVAL 1 DAY)
                          AND DATE_ADD(ft.training_date, INTERVAL 7 DAY)
         THEN fp.service_time_avg
-    END) as after_service_time,
+    END), 0) as after_service_time,
 
-    AVG(CASE
+    COALESCE(AVG(CASE
         WHEN fp.date BETWEEN DATE_ADD(ft.training_date, INTERVAL 1 DAY)
                          AND DATE_ADD(ft.training_date, INTERVAL 7 DAY)
         THEN fp.error_count
-    END) as after_error
+    END), 0) as after_error
 
 FROM fact_training AS ft
 LEFT JOIN dim_training AS dt
     ON ft.training_id = dt.training_id
 LEFT JOIN fact_performance AS fp
     ON ft.employee_id = fp.employee_id
+    AND fp.date BETWEEN 
+        DATE_SUB(ft.training_date, INTERVAL 7 DAY)
+        AND DATE_ADD(ft.training_date, INTERVAL 7 DAY)
+
 GROUP BY
-    ft.employee_id, ft.training_id, dt.training_name, ft.training_date;
+    ft.employee_id, ft.training_id, dt.training_name, ft.training_date
+HAVING before_orders IS NOT NULL AND after_orders IS NOT NULL;
 
 
 -- USE CASE 2 (generate data harian untuk visual trend performa sebelum dan sesudah training)
